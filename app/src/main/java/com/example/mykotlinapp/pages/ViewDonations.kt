b1d235de78b85997b137c4400a24aa16fa25c295
Wangui Margaret <148498603+margret255@users.noreply.github.com>
2025-01-09 18:34:26 +0300

    Update ViewDonations.kt

--- app/src/main/java/com/example/mykotlinapp/pages/ViewDonations.kt
+++ app/src/main/java/com/example/mykotlinapp/pages/ViewDonations.kt
@@ -9,6 +9,6 @@ import com.example.mykotlinapp.R
 class ViewDonations : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.view_donations) // Links to the view_donations.xml layout
+        setContentView(R.layout.view_donations) 
     }
 }
