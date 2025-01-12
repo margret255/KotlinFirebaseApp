fdfed0293803f2bb5f749958333b01808cb2f879
Wangui Margaret <148498603+margret255@users.noreply.github.com>
2025-01-12 11:24:06 +0300

    Update LoginPage.kt

--- app/src/main/java/com/example/mykotlinapp/pages/LoginPage.kt
+++ app/src/main/java/com/example/mykotlinapp/pages/LoginPage.kt
@@ -3,12 +3,21 @@ package com.example.mykotlinapp.pages
 
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mykotlinapp.R
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login) 
+        setContentView(R.layout.login)
+        val emailEditText: EditText = findViewById(R.id.editTextEmail)
+        val passwordEditText: EditText = findViewById(R.id.editTextPassword)
+        val loginButton: Button = findViewById(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+        }
     }
 }
