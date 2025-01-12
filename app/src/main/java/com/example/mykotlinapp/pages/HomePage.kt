e2135dc8d458a1fea87e6fc35890420109f3183f
Wangui Margaret <148498603+margret255@users.noreply.github.com>
2025-01-12 11:24:31 +0300

    Update HomePage.kt

--- app/src/main/java/com/example/mykotlinapp/pages/HomePage.kt
+++ app/src/main/java/com/example/mykotlinapp/pages/HomePage.kt
@@ -5,8 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.mykotlinapp.R
 
 class Homepage : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) { 
+    override fun onCreate(savedInstanceState: Bundle?) { // Fixed typo in 'savedInstanceState'
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home) 
+        setContentView(R.layout.home) // Links to the home.xml layout
     }
 }
+
